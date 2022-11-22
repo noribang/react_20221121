0787dc25e5cf902cd2a633ae76985c81cb73e1fe
hoonkim <hoonk9387@gmail.com>
2022-11-21 22:03:19 -0600

    Cleaned up App.js

--- src/App.js
+++ src/App.js
@@ -1,28 +1,10 @@
-// import logo from './logo.svg';
-// import './App.css';
 import React from "react";
-import { format } from "date-fns";
+// import { format } from "date-fns";
 
 function App() {
   return (
-    // <div className="App">
-    //   <header className="App-header">
-    //     <img src={logo} className="App-logo" alt="logo" />
-    //     <p>
-    //       Edit <code>src/App.js</code> and save to reload.
-    //     </p>
-    //     <a
-    //       className="App-link"
-    //       href="https://reactjs.org"
-    //       target="_blank"
-    //       rel="noopener noreferrer"
-    //     >
-    //       Learn React
-    //     </a>
-    //   </header>
-    // </div>
     <div className="App">
-      <h1>{format(new Date(), "MMMM do yyyy, h:mm:ss a")}</h1>
+      {/* <h1>{format(new Date(), "MMMM do yyyy, h:mm:ss a")}</h1> */}
       <p className="App-intro">
         In React apps, we write JSX - it looks like HTML, and uses a lot of HTML
         syntax. JSX lets us include JavaScript functions right along with the HTML,
